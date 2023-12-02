bf7068e5c265bbbb7146e824a0ab9df74565d518
blackestwhite <pesaregoal@gmail.com>
2023-12-02 13:07:14 +0330

    fix: infinite loop

--- gopenai.go
+++ gopenai.go
@@ -25,7 +25,7 @@ func SetupCustom(openAiKey string, client *http.Client) *GopenAiInstance {
 	return instance
 }
 
-func (h *GopenAiInstance) GenerateChatCompletion(prompt ChatCompletionRequestBody) (<-chan ChatCompletionChunk, error) {
+func (h *GopenAiInstance) GenerateChatCompletion(prompt ChatCompletionRequestBody) (chan ChatCompletionChunk, error) {
 	marshalled, err := json.Marshal(prompt)
 	if err != nil {
 		return nil, err
@@ -51,8 +51,13 @@ func (h *GopenAiInstance) GenerateChatCompletion(prompt ChatCompletionRequestBod
 		}
 		defer res.Body.Close()
 
+		if res.StatusCode != http.StatusOK {
+			log.Fatalf("Unexpected status code: %v", res.StatusCode)
+			return
+		}
+
 		scanner := bufio.NewScanner(res.Body)
-		for {
+		for scanner.Scan() {
 			line := scanner.Text()
 			if line == "" {
 				continue
@@ -61,7 +66,6 @@ func (h *GopenAiInstance) GenerateChatCompletion(prompt ChatCompletionRequestBod
 			var chunk ChatCompletionChunk
 			err = json.Unmarshal([]byte(line)[6:], &chunk)
 			if err != nil {
-				close(resultCh)
 				log.Printf("Error unmarshalling JSON: %v", err)
 				break
 			}
@@ -72,6 +76,12 @@ func (h *GopenAiInstance) GenerateChatCompletion(prompt ChatCompletionRequestBod
 				break
 			}
 		}
+
+		if err := scanner.Err(); err != nil {
+			log.Fatalf("Error reading response body: %v", err)
+		}
+
+		close(resultCh)
 	}()
 
 	return resultCh, nil
